f03d2ed7353337efa6aeadae18517e0c90332d7c
Pranav Tadimeti <47176383+PranavTadimeti@users.noreply.github.com>
2023-12-08 19:13:45 -0500

    minor fixes

--- nyc_green_taxi_data_commands.sql
+++ nyc_green_taxi_data_commands.sql
@@ -29,17 +29,22 @@ group by hour order by avg_num_trips desc;
 select avg(congestion_surcharge), hour(PU_datetime) as hour from green_taxi_data where year(PU_datetime) > 2018 
 group by hour(PU_datetime) order by avg(congestion_surcharge) desc limit 10;
 
--- highest pu and do zones
+-- highest pickup and dropoff zones
 select PU_zone, count(PU_zone) as pu_zone_cnt from green_taxi_data group by PU_zone order by pu_zone_cnt desc limit 10;
 select DO_zone, count(DO_zone) as do_zone_cnt from green_taxi_data group by DO_zone order by do_zone_cnt desc limit 10;
 
--- lowest pu and do zones
+-- lowest pickup and dropoff zones
 select PU_zone, count(PU_zone) as pu_zone_cnt from green_taxi_data group by PU_zone order by pu_zone_cnt asc limit 10;
 select DO_zone, count(DO_zone) as do_zone_cnt from green_taxi_data group by DO_zone order by do_zone_cnt asc limit 10;
 
 -- most common routes
 select (PU_zone, DO_zone), count((PU_zone,DO_zone)) as cnt from green_taxi_data group by (PU_zone, DO_zone) order by cnt desc limit 10;
 
--- pairs with highest discrepancy between PU and DO
-select t1.PU_zone, t1.cnt1, t2.cnt2 from (select PU_zone,count(PU_zone) as cnt1 from green_taxi_data group by PU_zone) as t1 JOIN (select PDO_zone,count(DO_zone) as cnt2 from green_taxi_data group by DO_zone) as t2 WHERE t1.PU_zone = t2.DO_zone;
+-- zones with highest discrepancy between pickup and dropoff
+select PU_zone as zone, abs(t1.cnt1 - t2.cnt2) as diff from (
+  (select PU_zone, count(PU_zone) as cnt1 from green_taxi_data group by PU_zone) as t1
+  INNER JOIN
+  ((select DO_zone, count(DO_zone) as cnt2 from green_taxi_data group by DO_zone) as t2)
+  ON t1.PU_zone = t2.DO_zone)
+  order by diff desc limit 10;
 
